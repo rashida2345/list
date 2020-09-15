17b56daa42acf3910b1fc1e391b9349fe68fa1f9
rashida2345 <70997999+rashida2345@users.noreply.github.com>
2020-09-15 11:27:58 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+number=0
+while(number<len(list1)):
+if list1[number]>=0:
+print(list1[number],end "")
+number +=1
+
+list2=[12,14,-95,3]
+number=0
+while(number<len(list2)):
+if list2[number]>=0:
+print(list2[number],end"")
+number +=1
